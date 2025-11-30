0de2a4cd9de5194b6eb6bfa1751a904ea7f0700c
Stefan <stefan.haubenthal@gmail.com>
2025-11-30 01:31:39 +0100

    Refactor pc-analog input driver code

--- input/pcanalog.s
+++ input/pcanalog.s
@@ -1,29 +1,203 @@
 ; GEOS by Berkeley Softworks
 ; reverse engineered by Maciej Witkowiak, Michael Steil
 ;
-; pcanalog input driver
+; pc-analog input driver
+
+.include "jumptab.inc"
 
 .segment "inputdrv"
 
-.byte $4c, $94, $fe, $4c, $a0, $fe, $4c, $a1, $fe, $00, $2d, $33, $39, $07, $05, $39
-.byte $3e, $43, $08, $04, $a9, $00, $85, $3b, $a9, $00, $85, $3a, $a9, $00, $85, $3c
-.byte $60, $24, $30, $10, $fb, $a5, $01, $48, $a9, $35, $85, $01, $ad, $00, $dc, $48
-.byte $ad, $02, $dc, $48, $ad, $03, $dc, $48, $a9, $ff, $8d, $02, $dc, $a9, $40, $8d
-.byte $00, $dc, $a2, $66, $ea, $ea, $ea, $ca, $d0, $fa, $ad, $19, $d4, $cd, $8a, $fe
-.byte $30, $08, $cd, $8c, $fe, $10, $3a, $4c, $42, $ff, $85, $02, $ad, $8b, $fe, $38
-.byte $e5, $02, $85, $02, $ad, $8d, $fe, $85, $04, $a9, $00, $85, $03, $85, $05, $a2
-.byte $02, $a0, $04, $20, $69, $c1, $a5, $3a, $38, $e5, $02, $85, $3a, $a5, $3b, $f0
-.byte $07, $e9, $00, $85, $3b, $4c, $42, $ff, $b0, $fb, $a9, $00, $85, $3a, $4c, $42
-.byte $ff, $38, $ed, $8b, $fe, $85, $02, $ad, $8e, $fe, $85, $04, $a9, $00, $85, $03
-.byte $85, $05, $a2, $02, $a0, $04, $20, $69, $c1, $a5, $3a, $18, $65, $02, $85, $3a
-.byte $a5, $3b, $69, $00, $85, $3b, $f0, $cd, $a5, $3a, $c9, $40, $30, $c7, $a9, $3f
-.byte $85, $3a, $ad, $1a, $d4, $cd, $8f, $fe, $30, $08, $cd, $91, $fe, $10, $32, $4c
-.byte $ab, $ff, $85, $02, $ad, $90, $fe, $38, $e5, $02, $85, $02, $ad, $92, $fe, $85
-.byte $04, $a9, $00, $85, $03, $85, $05, $a2, $02, $a0, $04, $20, $69, $c1, $a5, $3c
-.byte $38, $e5, $02, $90, $05, $85, $3c, $4c, $ab, $ff, $a9, $00, $85, $3c, $4c, $ab
-.byte $ff, $38, $ed, $90, $fe, $85, $02, $ad, $93, $fe, $85, $04, $a9, $00, $85, $03
-.byte $85, $05, $a2, $02, $a0, $04, $20, $69, $c1, $a5, $3c, $18, $65, $02, $c9, $c7
-.byte $b0, $05, $85, $3c, $4c, $ab, $ff, $a9, $c7, $85, $3c, $a9, $00, $8d, $02, $dc
-.byte $8d, $03, $dc, $ad, $01, $dc, $29, $0c, $cd, $89, $fe, $f0, $13, $8d, $89, $fe
-.byte $0a, $0a, $0a, $0a, $10, $01, $0a, $8d, $05, $85, $a5, $39, $09, $20, $85, $39
-.byte $68, $8d, $03, $dc, $68, $8d, $02, $dc, $68, $8d, $00, $dc, $68, $85, $01, $60
+MouseInit:
+	jmp     _MouseInit
+SlowMouse:
+	jmp     _SlowMouse
+UpdateMouse:
+	jmp     _UpdateMouse
+.ifdef bsw128
+SetMouse:
+	rts
+.endif
+
+LFE89:  .byte   $00
+LFE8A:  .byte   $2D
+LFE8B:  .byte   $33
+LFE8C:  .byte   $39
+LFE8D:  .byte   $07
+LFE8E:  .byte   $05
+LFE8F:  .byte   $39
+LFE90:  .byte   $3E
+LFE91:  .byte   $43
+LFE92:  .byte   $08
+LFE93:  .byte   $04
+
+_MouseInit:
+LFE94:  lda     #$00
+	sta     $3B
+	lda     #$00
+	sta     $3A
+	lda     #$00
+	sta     $3C
+_SlowMouse:
+LFEA0:  rts
+
+_UpdateMouse:
+LFEA1:  bit     $30
+	bpl     LFEA0
+	lda     $01
+	pha
+	lda     #$35
+	sta     $01
+	lda     $DC00
+	pha
+	lda     $DC02
+	pha
+	lda     $DC03
+	pha
+	lda     #$FF
+	sta     $DC02
+	lda     #$40
+	sta     $DC00
+	ldx     #$66
+LFEC4:  nop
+	nop
+	nop
+	dex
+	bne     LFEC4
+	lda     $D419
+	cmp     LFE8A
+	bmi     LFEDA
+	cmp     LFE8C
+	bpl     LFF11
+	jmp     LFF42
+
+LFEDA:  sta     $02
+	lda     LFE8B
+	sec
+	sbc     $02
+	sta     $02
+	lda     LFE8D
+	sta     $04
+	lda     #$00
+	sta     $03
+	sta     $05
+	ldx     #$02
+	ldy     #$04
+	jsr     Ddiv
+	lda     $3A
+	sec
+	sbc     $02
+	sta     $3A
+	lda     $3B
+	beq     LFF08
+	sbc     #$00
+	sta     $3B
+LFF05:  jmp     LFF42
+
+LFF08:  bcs     LFF05
+	lda     #$00
+	sta     $3A
+	jmp     LFF42
+
+LFF11:  sec
+	sbc     LFE8B
+	sta     $02
+	lda     LFE8E
+	sta     $04
+	lda     #$00
+	sta     $03
+	sta     $05
+	ldx     #$02
+	ldy     #$04
+	jsr     Ddiv
+	lda     $3A
+	clc
+	adc     $02
+	sta     $3A
+	lda     $3B
+	adc     #$00
+	sta     $3B
+	beq     LFF05
+	lda     $3A
+	cmp     #$40
+	bmi     LFF05
+	lda     #$3F
+	sta     $3A
+LFF42:  lda     $D41A
+	cmp     LFE8F
+	bmi     LFF52
+	cmp     LFE91
+	bpl     LFF81
+	jmp     LFFAB
+
+LFF52:  sta     $02
+	lda     LFE90
+	sec
+	sbc     $02
+	sta     $02
+	lda     LFE92
+	sta     $04
+	lda     #$00
+	sta     $03
+	sta     $05
+	ldx     #$02
+	ldy     #$04
+	jsr     Ddiv
+	lda     $3C
+	sec
+	sbc     $02
+	bcc     LFF7A
+	sta     $3C
+	jmp     LFFAB
+
+LFF7A:  lda     #$00
+	sta     $3C
+	jmp     LFFAB
+
+LFF81:  sec
+	sbc     LFE90
+	sta     $02
+	lda     LFE93
+	sta     $04
+	lda     #$00
+	sta     $03
+	sta     $05
+	ldx     #$02
+	ldy     #$04
+	jsr     Ddiv
+	lda     $3C
+	clc
+	adc     $02
+	cmp     #$C7
+	bcs     LFFA7
+	sta     $3C
+	jmp     LFFAB
+
+LFFA7:  lda     #$C7
+	sta     $3C
+LFFAB:  lda     #$00
+	sta     $DC02
+	sta     $DC03
+	lda     $DC01
+	and     #$0C
+	cmp     LFE89
+	beq     LFFD0
+	sta     LFE89
+	asl     a
+	asl     a
+	asl     a
+	asl     a
+	bpl     LFFC7
+	asl     a
+LFFC7:  sta     $8505
+	lda     $39
+	ora     #$20
+	sta     $39
+LFFD0:  pla
+	sta     $DC03
+	pla
+	sta     $DC02
+	pla
+	sta     $DC00
+	pla
+	sta     $01
+	rts
